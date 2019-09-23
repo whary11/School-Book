afcd1a45d6b0314f085566494f3f5304ce2f2056
Luis Fernando Raga <whary11@gmail.com>
2019-09-22 19:26:19 -0500

    Funcionalidad que permite agregar todos los permisos a un rol (switch)

--- resources/js/components/roles/ViewRole.vue
+++ resources/js/components/roles/ViewRole.vue
@@ -33,8 +33,24 @@
                 v-if="role_error.name.class.length > 0"
               >Error en el nombre.</div>
             </div>
+
+            <span>Añadir todos</span>
+            <div>
+              <label class="switch switch-3d switch-primary">
+                <input
+                  class="switch-input"
+                  type="checkbox"
+                  :checked="checked"
+                  @change="addAllPermissions"
+                />
+                <span class="switch-slider"></span>
+              </label>
+            </div>
             <div class="form-group">
-              <label for="name">Permisos</label>
+              <label for="name">
+                <span class="text-center">O seleccione los permisos para este rol</span>
+              </label>
+
               <v-select :options="permissions" label="name" v-model="role.permissions" multiple></v-select>
             </div>
             <!-- </form> -->
@@ -56,6 +72,7 @@ export default {
   props: ["role"],
   data() {
     return {
+      checked: false,
       permissions: [],
       url: false,
 
@@ -105,6 +122,13 @@ export default {
           .catch(error => {
             console.log(error);
           });
+      }
+    },
+    addAllPermissions(e) {
+      if (e.target.checked) {
+        this.role.permissions = this.permissions;
+      } else {
+        this.role.permissions = [];
       }
     }
   },
