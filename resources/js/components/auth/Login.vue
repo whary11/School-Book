5f00b0e1796116dabf9eb6e749ad82faa7a8207b
Luis Fernando Raga <whary11@gmail.com>
2019-10-06 20:54:38 -0500

    v-permisssios resive un array de permisos para validar

--- resources/js/components/auth/Login.vue
+++ resources/js/components/auth/Login.vue
@@ -49,7 +49,7 @@
                 <div>
                   <h2>Sign up</h2>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+                    Lorem ipsum sdolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
                     incididunt ut labore et dolore magna aliqua.
                   </p>
                   <button class="btn btn-primary active mt-3" type="button">Register Now!</button>
